d4a250937a47a2f7599fb87c046c34596d80239d
Nikola <nikola.novak9@gmail.com>
2016-05-31 22:22:11 +0200

    Add SDLMoodCard with Xib

---
+++ Loods/Source/Classes/Models/SDLMoodCard.h
@@ -0,0 +1,16 @@
+//
+//  SDLMoodCard.h
+//  Loods
+//
+//  Created by Nikola on 31/05/16.
+//  Copyright © 2016 SixtyDegrees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDLMoodCard : UIView
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewMoodCard;
+@property (weak, nonatomic) IBOutlet UILabel *labelMoodTitle;
+
+@end
---
+++ Loods/Source/Classes/Models/SDLMoodCard.m
@@ -0,0 +1,21 @@
+//
+//  SDLMoodCard.m
+//  Loods
+//
+//  Created by Nikola on 31/05/16.
+//  Copyright © 2016 SixtyDegrees. All rights reserved.
+//
+
+#import "SDLMoodCard.h"
+
+@implementation SDLMoodCard
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
